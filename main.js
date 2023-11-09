fa76e4697bcecfa0758b19883b5fae3365fa97dd
Patrik-Bjorkman <patrikbman94@gmail.com>
2023-11-09 08:10:21 +0100

    commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const button = document.getElementById('button'); // Retrieve the button element using the getElementById() method
+const emptyDiv = document.getElementById('btn'); // Retrieve the div element using the querySelector() method
+const colorInput = document.getElementById('color'); // Retrieve the input element using the getElementById() method
+const contentInput = document.getElementById('content'); // Retrieve the input element using the getElementById() method
+const divStyleCheckbox = document.getElementById('divStyle'); // Retrieve the input element using the getElementById() method
+const checkbox = document.querySelector('input[type="checkbox"]'); // Retrieve the input element using the querySelector() method
+const textFields = document.getElementsByClassName('textfield'); // Retrieve the input elements using the getElementsByClassName() method
+
+button.addEventListener('click', function() {       // Add a click event listener to the button element
+    if (emptyDiv.style.display === 'none') {        // Check if the display property of the div element is set to none
+        emptyDiv.style.display = 'block';
+    } else {                                        // If the display property of the div element is not set to none
+        emptyDiv.style.display = 'none';
+    }
+});
+colorInput.addEventListener('input', function() { // Add an input event listener to the input element
+    emptyDiv.style.backgroundColor = colorInput.value; // Set the background color of the div element to the value of the input element
+});
+contentInput.addEventListener('input', function() { // Add an input event listener to the input element
+    emptyDiv.textContent = contentInput.value; // Set the text content of the div element to the value of the input element
+});
+divStyleCheckbox.addEventListener('change', function() { // Add a change event listener to the input element
+    if (divStyleCheckbox.checked) { // Check if the checkbox is checked
+        emptyDiv.classList.add('border-rounded');
+        emptyDiv.style.backgroundColor = colorInput.value; // Set the background color of the div element to the value of the input element
+    } else { // If the checkbox is not checked
+        emptyDiv.classList.remove('border-rounded');
+    }
+});
